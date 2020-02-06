75b3e4a252eb6a1893d43d9380a78fa25e1c20db
Cesar <cesar2012oliveira@gmail.com>
2020-02-06 01:07:40 -0300

    typed.js added to aplication

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,4 +1,6 @@
-import React from "react"
+import React, { useEffect } from "react"
+
+import Typed from 'typed.js'
 
 // Style
 import "./hero.scss"
@@ -6,11 +8,24 @@ import "./hero.scss"
 import appImage from "images/appWhite.svg"
 
 const Hero = () => {
+  useEffect(() => {
+    console.log('oi')
+    const typed = new Typed('.hero .featured-text', {
+      strings: ["gritar", "torcer", "pular", 'xingar', "vibrar", "se exaltar", "ficar puto", "pistolar", "paixão"],
+      typeSpeed: 50,
+      typeSpeed: 80,
+      backSpeed: 50,
+      shuffle: true,
+      backDelay: 3000,
+      loop: true,
+      showCursor: false
+    });
+  }, [])
   return (
     <section className="hero">
       <div className="container">
         <h1 className="title">
-          ser torcedor é <strong>cantar</strong>
+          ser torcedor é <strong className="featured-text">cantar</strong>
         </h1>
         <img src={appImage} className="image-featured" />
       </div>
