b98eb10ed0b7a6599d0cf60b9634f93b6e2ab148
Cesar <cesar2012oliveira@gmail.com>
2020-02-06 00:23:36 -0300

    some mobile adjustments

--- src/components/ContentBlock/index.js
+++ src/components/ContentBlock/index.js
@@ -4,7 +4,7 @@ const ContentBlock = ({ title = "", paragraphs = [], customClass = "" }) => {
   return (
     <>
       <h1 className="title">{title}</h1>
-      <div className="content">
+      <div className="holder">
         {paragraphs.map((item, index) => (
           <p className="paragraph" key={index}>
             {item}
