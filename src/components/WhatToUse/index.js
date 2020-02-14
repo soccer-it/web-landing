62ec6b57401c058f767d2ea4b47fad637568ce01
Cesar <cesar2012oliveira@gmail.com>
2020-02-13 23:43:23 -0300

    gsap, scrollmagic and tweens added to project via simple cdn

--- src/components/WhatToUse/index.js
+++ src/components/WhatToUse/index.js
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { useEffect } from "react"
+
+// import { TimelineMax, TweenMax, Linear } from 'gsap';
+// import ScrollMagic from 'scrollmagic';
+// import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 
 // Style
 import "./what-to-use.scss"
 
 const WhatToUse = () => {
+  useEffect(() => {
+    const controller = new window.ScrollMagic.Controller()
+
+    const scene = new window.ScrollMagic.Scene({
+      triggerElement: "#whattouse",
+      duration: 300,
+    })
+      // animate color and top border in relation to scroll position
+      .setTween(".list", {
+        scale: 0.7,
+      }) // the tween durtion can be omitted and defaults to 1
+      // .addIndicators({ name: "2 (duration: 300)" }) // add indicators (requires plugin)
+      .addTo(controller)
+
+    console.log("opa", scene)
+  }, [])
   return (
-    <section className="what-to-use"  id="whattouse">
+    <section className="what-to-use" id="whattouse">
       <div className="container">
         <h1 className="title">recursor disponíveis</h1>
       </div>
@@ -57,7 +77,6 @@ const WhatToUse = () => {
         <li className="item">escalação ao vivo</li>
         <li className="item">alertas de transmissão</li>
       </ul>
-      
     </section>
   )
 }
